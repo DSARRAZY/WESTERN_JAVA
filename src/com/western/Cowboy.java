ca0af650c05e2a36e2cf27770269c067c296aa23
DSARRAZY <d.sarrazy@gmail.com>
2021-05-27 16:13:28 +0200

    fin des classes

--- src/com/western/Cowboy.java
+++ src/com/western/Cowboy.java
@@ -1,4 +1,27 @@
 package com.western;
 
-public class Cowboy {
+public class Cowboy extends Humain{
+
+    private String nom;
+    private int popularite;
+    private String adjectif = "vaillant";
+
+    // Constructeur
+    public Cowboy(String unNom, String uneBoisson, int unePopularite, String unAdjectif) {
+        super(unNom, uneBoisson);
+        nom = unNom;
+        popularite = unePopularite;
+        adjectif = unAdjectif;
+    }
+
+    // Méthodes
+    public String libererDame(Dame UneDame) {
+        return "Je me suis débarrasser de lui, vous êtes libre maintenant.";
+    }
+
+    public String tirerBrigand(Brigand unBrigand) {
+        return "Le " + adjectif + " " + nom + "tire sur " + unBrigand.quelEstTonNom() +". PAN !";
+        //return "Prend ça, rascal !";
+    }
+
 }
--- src/com/western/Histoire.java
+++ src/com/western/Histoire.java
@@ -9,6 +9,14 @@ public class Histoire {
         System.out.println(personnage.presentation());
         System.out.println(personnage.boire());
 
+        Brigand personnage2 = new Brigand("Joe Dalton","whisky", "cool",1,50,false);
+        System.out.println(personnage2.presentation());
+
+        Cowboy personnage3 = new Cowboy("Alfred","vin",0,"vaillant");
+        System.out.println(personnage3.presentation());
+
+        Dame personnage4 = new Dame ("Lady Marianne", "eau gazeuse","rose","libre");
+        System.out.println(personnage4.presentation());
 
     }
 }
